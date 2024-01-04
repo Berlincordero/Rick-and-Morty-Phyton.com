0565e4c166e73a1dca2e4bb71a59d50226d15a6d
Berlincordero <114275282+Berlincordero@users.noreply.github.com>
2024-01-03 20:28:07 -0600

    first commit

---
+++ Morty.py
@@ -0,0 +1,16 @@
+#A diferencia de otros lenjuages phyton no lleva (;) 
+#print para imprimir una variable.
+#Hola Morty
+print('hello Morty')
+
+
+#para consultar diferentes tipos de datos 
+#ejemplos
+#string
+print(type('hello Morty')) # tipo 'str'
+#int
+print(type(5)) # tipo 'int'
+#float
+print(type(1.5)) #tipo 'float'
+#booleano
+print(type(True)) #tipo 'bool'
